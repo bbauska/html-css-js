03a63f6edf1c6f6daf1f64162c221d38a5a1fbf1
Makavura <makavuramughanga@gmail.com>
2020-09-01 16:55:03 +0300

    documentation: create a modular collection of links and basic information if need be

---
+++ fundamentals.js
@@ -0,0 +1,102 @@
+/* 
+
+This file  contains an introduction into the fundamentals of Javascript.
+It is broken down into modular sections preceeded by module title in CAPS, 
+only the titles are written in "FULL CAPS".
+
+A breakdown of modules
+
+Pseudocode
+Values and Types
+Scope
+Control Flow
+Functions
+Data Structures & Algorithms
+Modules
+DOM
+Events
+Forms
+NodeJS
+
+*/
+
+
+/* 
+PSEUDOCODE
+
+ pseudocode is a plain language description of the steps in an algorithm or another system
+ link: https://en.wikipedia.org/wiki/Pseudocode
+
+*/
+
+
+/* 
+VALUES,  TYPES & OPERATORS
+    Values - Chunks that represent pieces of information
+
+    Operators 
+    Unary - take one value
+    e.g typeof(f)
+
+    Binary - use two values
+    e.g.addition +
+        subtraction -
+        division /
+        multiplication *
+
+    NB: For more checkout https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
+
+Numbers e.g 1
+String e.g 'Piece of Text' or "Piece of Text" or `Pieces of Text`
+Boolean i.e true or false
+empty values -undefined and null
+
+*/
+
+/* 
+SCOPE
+Local and global 
+
+Checkout https://www.digitalocean.com/community/tutorials/understanding-variables-scope-hoisting-in-javascript#:~:text=Scope%20in%20JavaScript%20refers%20to,declared%20inside%20of%20a%20block 
+
+a Great article that explains in concise detail what and how
+*/
+
+/* 
+CONTROL FLOW
+https://medium.com/javascript-in-plain-english/introduction-to-javascript-control-flow-6272f92b75fa
+
+https://exploringjs.com/impatient-js/ch_control-flow.html
+*/
+
+
+/* 
+Functions
+https://eloquentjavascript.net/03_functions.html
+*/
+
+/* 
+Module
+https://eloquentjavascript.net/10_modules.html
+*/
+
+/* 
+Data Structures & Algorithms
+https://eloquentjavascript.net/04_data.html
+*/
+
+/* 
+DOM
+https://www.w3schools.com/js/js_htmldom.asp
+*/
+
+/* 
+EVENTS
+https://www.w3schools.com/js/js_events.asp
+*/
+
+/* 
+FORMS
+https://eloquentjavascript.net/18_http.html
+*/
+
