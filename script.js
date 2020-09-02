4d6231741a0e2362b1e458b8d0ca0efd4ea91a01
Makavura <makavuramughanga@gmail.com>
2020-09-02 12:33:03 +0300

    chore: restore base backend

---
+++ script.js
@@ -0,0 +1,46 @@
+/* 
+PLACE CHALLENGE CODE IN HERE FOR BACKEND
+
+*/
+
+/* 
+DEV ENVIRONMENT SETUP
+
+npm init
+npm install http-server --save --g
+npm install express --save
+*/
+
+const express =  require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+/* 
+USE DATA STRUCTURES TO ENSURE DATA PERSISTENCE
+
+*/
+
+let submissions = [
+
+];
+
+
+
+app.get('/', (res) => {
+    res.send(`FUNDAMENTALS OF WEB DEVELOPMENT | CRUD`)
+})
+
+app.get('/submissions', (res) => {
+    res.json(submissions)
+})
+
+app.get('/submissions/:id', (res) => {
+    const id = req.params.id;
+    res.json(submissions[id])
+})
+
+app.post('/submission', (req, res) => {
+    submissions.push(req.body);
+    res.json(submissions[submissions.length -1]);
+})
+
